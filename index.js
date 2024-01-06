ab63e4f811a05bdb35d76a0d340ad8cf209941b0
Hriday Mehra <hridaymehra3@gmail.com>
2024-01-06 19:46:31 +0530

    Adding Animation and Adjusting that by an Even Interval

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
+const circle = document.querySelector('circle');
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-    onStart(){
+    onStart(totalDuration){
         console.log('Timer Started');
+        duration = totalDuration;
     },
-    onTick(){
+    onTick(timeRemaining){
         console.log('Timer just ticked Down!!');
+        circle.setAttribute('stroke-dashoffset', 
+            perimeter * timeRemaining / duration - perimeter
+        );
     },
     onComplete(){
         console.log('Timer Completed');
